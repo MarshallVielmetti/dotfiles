4058e0e2a6697db066048901d7d1a3d42b8e2f0b
Marshall Vielmetti <mvielmetti@outlook.com>
2024-09-29 13:19:34 -0500

    added trouble to base config

--- nvchad_ubuntu/.config/nvim/lua/plugins/init.lua
+++ nvchad_ubuntu/.config/nvim/lua/plugins/init.lua
@@ -6,7 +6,43 @@ return {
       require "configs.conform"
     end,
   },
-
+  {
+    "folke/trouble.nvim",
+    opts = {}, -- for default options, refer to the configuration section for custom setup.
+    cmd = "Trouble",
+    keys = {
+      {
+        "<leader>tx",
+        "<cmd>Trouble diagnostics toggle<cr>",
+        desc = "Diagnostics (Trouble)",
+      },
+      {
+        "<leader>tX",
+        "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+        desc = "Buffer Diagnostics (Trouble)",
+      },
+      {
+        "<leader>ts",
+        "<cmd>Trouble symbols toggle focus=false<cr>",
+        desc = "Symbols (Trouble)",
+      },
+      {
+        "<leader>tl",
+        "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+        desc = "LSP Definitions / references / ... (Trouble)",
+      },
+      {
+        "<leader>tL",
+        "<cmd>Trouble loclist toggle<cr>",
+        desc = "Location List (Trouble)",
+      },
+      {
+        "<leader>tQ",
+        "<cmd>Trouble qflist toggle<cr>",
+        desc = "Quickfix List (Trouble)",
+      },
+    },
+  },
   {
     "neovim/nvim-lspconfig",
     config = function()
@@ -25,7 +61,7 @@ return {
         "css-lsp",
         "prettier",
         "clangd",
-        "clang-format"
+        "clang-format",
       },
     },
   },
@@ -33,8 +69,8 @@ return {
     "jose-elias-alvarez/null-ls.nvim",
     event = "VeryLazy",
     opts = function()
-        return require "configs.null-ls"
-    end
+      return require "configs.null-ls"
+    end,
   },
   {
     "nvim-treesitter/nvim-treesitter",
@@ -59,7 +95,7 @@ return {
         enable_rename = true,
         enable_close = true,
         enable_close_on_slash = true,
-        filetypes = { "html", "xml", "js", "ts", "jsx", "tsx", "javascript"},
+        filetypes = { "html", "xml", "js", "ts", "jsx", "tsx", "javascript" },
       },
     },
   },
